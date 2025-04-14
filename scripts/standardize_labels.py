e33e55980d8dae7308349feeada7a43a9c774560
ScarletAnnaZ <zzaa92997@163.com>
2025-04-14 14:02:49 +0200

    Extract categories from excel to lables

--- scripts/standardize_labels.py
+++ scripts/standardize_labels.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import json
-import re  # 用于正则表达式处理
+import re  
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
@@ -22,13 +22,13 @@ def load_excel_labels(excel_path):
 
 def standardize_category_label(category):
     """
-    从 category 标签中提取括号内的内容。
-    如果不存在括号内容，则保留原标签。
+    extracte the content of the parentheses,
+    if no parentheses exist, the original label retained
     """
     match = re.search(r'\((.*?)\)', category)
     if match:
-        return match.group(1).strip()  # 提取括号内的内容并去除两端空格
-    return category.strip()  # 如果不存在括号内容，则返回原始标签
+        return match.group(1).strip()  # Extract the contents of the parentheses and remove the Spaces at both ends，
+    return category.strip()  # if no parentheses, back to original labels
 
 def save_labels_as_json(labels_df, output_path):
     labels_dict = {}
@@ -40,19 +40,19 @@ def save_labels_as_json(labels_df, output_path):
         
         labels_dict[motion_id] = {
             "description": description,
-            "category": category  # 使用标准化后的标签
+            "category": category  
         }
 
-    # 保存为 JSON 文件
+    # store as JSON file
     with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(labels_dict, f, indent=4, ensure_ascii=False)
 
     print(f"✅ Labels saved as JSON to {output_path}")
 
 if __name__ == "__main__":
-    # 加载 Excel 标签
+    # Excel labels
     labels_df = load_excel_labels(EXCEL_PATH)
     
-    # 保存为 JSON 文件
+    # as json 
     json_output_path = os.path.join(OUTPUT_DIR, 'standardized_labels.json')
     save_labels_as_json(labels_df, json_output_path)
