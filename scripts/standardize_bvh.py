d9068941905335af63eae730ad797817e499ed3b
ScarletAnnaZ <zzaa92997@163.com>
2025-05-08 15:35:58 +0200

    update

--- scripts/standardize_bvh.py
+++ scripts/standardize_bvh.py
@@ -5,7 +5,7 @@ from bvh import Bvh
 # Project root
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
-OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
+OUTPUT_DIR = os.path.join(BASE_DIR, 'output', 'processed600_bvh')
 
 # normalizing parameter
 TARGET_FPS = 120
